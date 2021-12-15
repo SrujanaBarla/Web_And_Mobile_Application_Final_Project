9d494297fef271e8f4b1f6c6d5b900b8a84f73a5
SrujanaBarla <89660593+SrujanaBarla@users.noreply.github.com>
2021-12-14 19:59:02 -0600

    Add files via upload

---
+++ Client/src/app/customer/services/customer.ts
@@ -0,0 +1,19 @@
+export class Customer {
+  customer_id: number;
+  customer_employee_id: string;
+  customer_working_days: string;
+  customer_basic: string;
+  customer_hra: string;
+  customer_mediclaim: string;
+  customer_ta: string;
+  customer_da: string;
+  customer_reimbursement: string;
+  customer_ca: string;
+  customer_others: string;
+  customer_dpf: string;
+  customer_dtax: string;
+  customer_desc: string;
+  customer_month: string;
+  customer_total: string;
+  customer_dedc: string;
+}
